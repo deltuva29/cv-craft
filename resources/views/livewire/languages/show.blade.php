5f755d6fb9e0828b25dd2a514d6a41890e294514
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2024-02-09 01:12:50 +0200

    fix: languages show

---
+++ resources/views/livewire/languages/show.blade.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\Profile;
+use Livewire\Attributes\On;
+use Livewire\Volt\Component;
+use Livewire\WithPagination;
+use Illuminate\Contracts\View\View;
+
+new class extends Component {
+    public Profile $profile;
+
+    public function mount(Profile $profile): void
+    {
+        sleep(1);
+        $this->profile = $profile;
+    }
+
+    #[On('profile-updated')]
+    public function update(): void
+    {
+        $this->dispatch('$refresh');
+    }
+
+    public function placeholder(): View
+    {
+        return view('placeholders.bubble');
+    }
+}; ?>
+
+<div>
+    @if (!$this->profile->languages->isEmpty())
+        <livewire:profile.update-profile-languages-form
+                :profile="$this->profile"
+        />
+    @else
+        <div x-data="{ empty: false }">
+            <div
+                    @profile-updated.window="empty = true"
+                    x-show="!empty"
+                    x-cloak
+            >
+                <x-empty-content
+                        title="{{ __('You have not added any languages yet.') }}"
+                />
+            </div>
+        </div>
+    @endif
+</div>
