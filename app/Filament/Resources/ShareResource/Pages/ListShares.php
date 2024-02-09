d605b63492d3e822e9f7998ebe73a99bb53c44c8
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2024-02-09 22:45:57 +0200

    added shares resource tabs filter

--- app/Filament/Resources/ShareResource/Pages/ListShares.php
+++ app/Filament/Resources/ShareResource/Pages/ListShares.php
@@ -2,7 +2,10 @@
 
 namespace App\Filament\Resources\ShareResource\Pages;
 
+use App\Enums\ShareTemplate;
 use App\Filament\Resources\ShareResource;
+use App\Models\Share;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListShares extends ListRecords
@@ -13,6 +16,21 @@ class ListShares extends ListRecords
     {
         return [
             //
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->badge(Share::query()->count()),
+            'default' => Tab::make()
+                ->modifyQueryUsing(fn($query) => $query->where('template', ShareTemplate::Default))
+                ->badge(
+                    Share::query()
+                        ->where('template', ShareTemplate::Default)
+                        ->count()
+                ),
         ];
     }
 }
