5c9ffcac2ff5d58d8a65084f1457d196644f6cb4
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2024-02-18 01:35:14 +0200

    fix: hero page

--- resources/views/livewire/pages/home/hero.blade.php
+++ resources/views/livewire/pages/home/hero.blade.php
@@ -7,19 +7,38 @@ new class extends Component {
 }; ?>
 
 <div>
-    <div class="relative overflow-hidden px-3" style="background: linear-gradient(90deg, #ECEEF5 0.14%, rgba(236, 238, 245, 0.00) 99.87%);">
-        <main class="mt-16 mx-auto max-w-7xl px-4">
+    <div class="relative overflow-hidden px-3">
+        <main class="mt-16 mx-auto max-w-4xl px-4">
             <div class="flex flex-col items-center justify-center">
-                <h1 class="text-3xl text-center font-mono font-extrabold text-gray-900 dark:text-gray-100 sm:text-7xl">
+                <h1 class="text-4xl text-center font-mono font-extrabold text-gray-900 dark:text-gray-100 sm:text-7xl">
                     <x-text-reveal>
-                        {!! __('Find your next <strong class="text-secondary">:job</strong>!', ['job' => __('job')]) !!}
+                        {!! __('Create a new resume with <strong class="text-secondary">:name</strong>!', ['name' => config('app.name')]) !!}
                     </x-text-reveal>
                 </h1>
                 <p class="mt-4 text-center text-lg text-gray-500 dark:text-gray-400">
                     <x-text-reveal>
-                        {!! __('“LaraJobs is our first stop whenever we\'re hiring a Laravel role. We\'ve hired 10 Laravel developers in the last few years, all thanks to LaraJobs.” — Matthew Hall, ArborXR') !!}
+                        {!! __('The simplest way to create a new resume.') !!}
                     </x-text-reveal>
                 </p>
+            </div>
+
+            <div class="my-12">
+                <div class="flex flex-col justify-center items-center space-y-2">
+                    <x-nav-link
+                            :href="route('login')"
+                            class="w-full sm:w-1/2 flex justify-center items-center !px-4 !py-4 bg-secondary dark:bg-secondary border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-primary/75 dark:hover:bg-white focus:bg-primary/75 dark:focus:bg-primary/75 active:bg-primary/75 dark:active:bg-primary/75 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                            wire:navigate
+                    >
+                        {{ __('Log In') }}
+                    </x-nav-link>
+                    <x-nav-link
+                            :href="route('register')"
+                            class="w-full sm:w-1/2 flex justify-center items-center !px-4 !py-4 bg-secondary dark:bg-secondary border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-primary/75 dark:hover:bg-white focus:bg-primary/75 dark:focus:bg-primary/75 active:bg-primary/75 dark:active:bg-primary/75 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                            wire:navigate
+                    >
+                        {{ __('Create Account') }}
+                    </x-nav-link>
+                </div>
             </div>
         </main>
     </div>
