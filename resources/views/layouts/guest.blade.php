bacc3e545fcba58883a095a86f7126dd6a483db8
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2024-02-18 01:44:29 +0200

    fix: guest applicaton logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,12 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
+    <style>
+        strong {
+            color: #00A36C !important;
+        }
+    </style>
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
