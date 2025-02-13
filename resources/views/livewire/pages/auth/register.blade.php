9b6a01a1b8108fc4138cec59ac34f07267f2a2e1
Mindaugas Deltuva <deltuva.mindaugas@gmail.com>
2025-02-13 09:52:08 +0200

    fix: hcaptcha finally

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -86,6 +86,8 @@ new #[Layout('layouts.guest')] class extends Component {
             <x-input-error :messages="$errors->get('hcaptcha')" class="mt-2"/>
         </div>
 
+        <script src="https://js.hcaptcha.com/1/api.js?hl=lt" async defer></script>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
                 {{ __('Already registered?') }}
